9285c27c5d64c80f686b06856c966d9bc030038b
Richard <rshendershot@gmail.com>
2014-02-05 10:08:05 -0600

    update conversion script

--- tools/rokuencode.sh
+++ tools/rokuencode.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
 
+renice +15 --pid $$
+
 #convert mpeg file to mp4 using handbrakecli
- 
-newname=`echo $2 | sed 's/\(.*\)\..*/\1/'`
-newname="$1/$newname.mp4"
+MYTHDIR=$1
+MPGFILE=$2
 
-/usr/bin/HandBrakeCLI -i $1/$2 -o $newname -e x264 -b 1500 -E faac -B 256 -R 48 -w 720
+DATABASEUSER=mythtv
+DATABASEPASSWORD=mythtv
 
+newbname=`echo $MPGFILE | sed 's/\(.*\)\..*/\1/'`
+newname="$MYTHDIR/$newbname.mp4"
 
+/usr/bin/HandBrakeCLI --preset='iPhone & iPod Touch' -i $MYTHDIR/$MPGFILE -o $newname 
+
+# update the db to point to the mp4
+NEWFILESIZE=`du -b "$newname" | cut -f1`
+echo "UPDATE recorded SET basename='$newbname.mp4',filesize='$NEWFILESIZE' WHERE basename='$2';" > /tmp/update-database.sql
+mysql --user=$DATABASEUSER --password=$DATABASEPASSWORD mythconverg < /tmp/update-database.sql
+
+# update the seek table
+#mythcommflag --file $MYTHDIR/$MPGFILE --rebuild
+
+# remove the orignal mpg
+#rm $MYTHDIR/$MPGFILE
